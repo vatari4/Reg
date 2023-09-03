e68ae78a91e29dbbac1317de8bb06dff4749c74c
mehak <mehaker.cool@gmail.com>
2023-09-04 01:11:53 +0500

    suka blyat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [mail, setMail] = useState('');
+  const [firstName, setName] = useState('');
+  const [email, setMail] = useState('');
+  const [password, setPassword] = useState('');
   const [response, setResponse] = useState('');
 
   const sendJSON = () => {
     const url = 'http://mihailmaximov.ru/projects/json/json.php';
-    const data = JSON.stringify({ name, mail });
+    const data = JSON.stringify({ firstName, email, password });
 
     fetch(url, {
       method: 'POST',
@@ -16,19 +17,20 @@ const App = () => {
       },
       body: data
     })
-    .then(response => response.text())
-    .then(data => {
-      setResponse(data);
-    })
-    .catch(error => {
-      console.error('Ошибка:', error);
-    });
+      .then(response => response.text())
+      .then(data => {
+        setResponse(data);
+      })
+      .catch(error => {
+        console.error('Ошибка:', error);
+      });
   };
 
   return (
     <div>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Name" />
-      <input type="text" value={mail} onChange={e => setMail(e.target.value)} placeholder="Mail" />
+      <input type="text" value={firstName} onChange={e => setName(e.target.value)} placeholder="firstName" />
+      <input type="text" value={email} onChange={e => setMail(e.target.value)} placeholder="email" />
+      <input type="text" value={password} onChange={e => setPassword(e.target.value)} placeholder="password" />
 
       <button onClick={sendJSON}>Проверить JSON</button>
 
